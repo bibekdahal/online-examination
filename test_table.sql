33eaa2517179e3852dd456f55cd3b0a08a4b08b0
Bibek Dahal <bibekdahal_bd16@hotmail.com>
2014-06-14 13:07:08 +0545

    Sql databse test

---
+++ test_table.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 14, 2014 at 09:21 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `test_online_exam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test_table`
+--
+
+CREATE TABLE IF NOT EXISTS `test_table` (
+  `Hello` int(11) NOT NULL,
+  `ASAS` int(11) NOT NULL,
+  `asfwqe` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `test_table`
+--
+
+INSERT INTO `test_table` (`Hello`, `ASAS`, `asfwqe`) VALUES
+(2, 3, 4),
+(2, 3, 4),
+(2, 3, 4);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
