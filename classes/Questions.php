aee67e15ab37cb63deffb7d254456e570e129817
Bibek Dahal <bibekdahal_bd16@hotmail.com>
2014-06-17 16:21:20 +0545

    Questions Database

--- classes/Questions.php
+++ classes/Questions.php
@@ -13,55 +13,45 @@ class Questions{
 		$this->m_sqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 		if ($this->m_sqli->connect_error)
 			trigger_error('Connect Error: ' . $this->m_sqli->connect_error);
-		$this->m_sqli->query('CREATE TABLE IF NOT EXISTS question_sets(setid int auto_increment, tablename varchar(100), primary key(setid))');
 	}
 	
 	public function __destruct(){
 		$this->m_sqli->close();
 	}
 	
-	public function NewQuestionSet(int $numberOfQuestions){
+	public function NewQuestionSet(string $imagefolder){
 		$mysqli = $this->m_sqli;
-		
-		if ($result = $mysqli->query('SELECT * FROM question_sets')){
-			$nsets = $result->num_rows;
-			$newname = "question".$nsets;
-			if ($res = $mysqli->query('CREATE TABLE '.$newname.'(id int auto_increment, question mediumtext, optiona mediumtext, optionb mediumtext, optionc mediumtext, optiond mediumtext, primary key(id))')){
-				for ($i=0; $i<$numberOfQuestions; $i++)
-					$mysqli->query('INSERT INTO '.$newname.' VALUES('.($i+1).', " ", " ", " ", " ")');
-				
-				if ($stmt1 = $mysqli->prepare('INSERT INTO question_sets(tablename) VALUES(?)')){
-					$stmt1->bind_param('s', $newname);
-					$stmt1->execute();
-					$stmt1->close();
-				}
-				$this->m_lastSetId = $stmt->insert_id;
-				
-				$res->close();
-			}
-			$result->close();
+
+		if ($stmt = $mysqli->prepare('INSERT INTO question_sets(imagesfolder) VALUES(?)')){
+			$stmt->bind_param('s', $imagefolder);
+			$stmt->execute();				
+			$this->m_lastSetId = $stmt->insert_id;
+			$stmt->close();	
 		}
+		
+		$res->close();
 	}
 	
-	public function UpdateQuestion(int $setIndex, int $questionIndex, 
-	string $question, string $optiona, string $optionb, string $optionc, string $optiond){
+	public function GetLastSetId() { return $this->m_lastSetId; }
+	
+	public function AddQuestion(int $setid, int $sn, string $question, string $optiona, string $optionb, string $optionc, string $optiond){
+		$mysqli = $this->m_sqli;
+
+		if ($stmt = $mysqli->prepare('INSERT INTO questions(setid, sn, question, optiona, optionb, optionc, optiond) VALUES(?, ?, ?, ?, ?, ?, ?)')){
+			$stmt->bind_param('iisssss', $setid, $sn, $question, $optiona, $optionb, $optionc, $optiond);
+			$stmt->execute();				
+			$stmt->close();	
+		}
+		
+		$res->close();
+	}
+	
+	public function UpdateQuestion(int $setid, int $sn, string $question, string $optiona, string $optionb, string $optionc, string $optiond){
 		$mysqli = $this->m_sqli;
 				
-		if ($stmt = $mysqli->prepare('SELECT tablename FROM question_sets WHERE setid=?')){
-			$stmt->bind_param('i', $setIndex);
-			$stmt->excute();
-			$stmt->bind_result($tablename);
-			
-			if ($stmt->fetch())
-			{
-				if ($stmt1 = $mysqli->prepare('UPDATE '.$tablename.' SET question=?,optiona=?,optionb=?,optionc=?,optiond=? WHERE id=?'))
-				{
-					$stmt1->bind_param('sssssi', $question, $optiona, $optionb, $optionc, $optiond, $questionIndex);
-					$stmt1->execute();
-					$stmt1->close();
-				}
-			}
-			
+		if ($stmt = $mysqli->prepare('UPDATE questions SET question=?, optiona=?, optionb=?, optionc=?, optiond=? WHERE setid=? AND $sn=?')){
+			$stmt->bind_param('sssssii', $question, $optiona, $optionb, $optionc, $optiond, $setid, $sn);
+			$stmt->execute();
 			$stmt->close();
 		}
 	}
